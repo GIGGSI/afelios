e94974a550a42baab47ddd80d7ace76992708284
Stoyan Spasov <spasovstoyan@gmail.com>
2023-01-31 23:51:49 +0200

    add info section

---
+++ src/components/InfoSection/InfoSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './InfoSection.css'
+const InfoSection = () => {
+    return (
+        <div className='info-wrapper'>
+            <h1 className='animate-charcter'>Безплатна доставка за Благоевград и Банско</h1>
+
+        </div>
+    )
+}
+
+export default InfoSection
