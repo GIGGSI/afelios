c28a62e871b6be4d161a6d054e03e5023b6e289c
Stoyan Spasov <spasovstoyan@gmail.com>
2023-05-05 19:13:47 +0300

    add move image

--- src/components/Products/ProductsConstants.js
+++ src/components/Products/ProductsConstants.js
@@ -1,36 +1,64 @@
-import unflavoured from '../../assets/unflavoured.jpeg'
-import greenApple from '../../assets/green-apple.jpeg';
-import melon from '../../assets/melon.jpeg'
+// import unflavoured from '../../assets/unflavoured.jpeg'
+// import greenApple from '../../assets/green-apple.jpeg';
+// import melon from '../../assets/melon.jpeg'
 import ballon from '../../assets/balon.JPG'
+import blubery from '../../assets/blueberry.webp'
+import lemon from '../../assets/lemon.webp';
+import watermelon from '../../assets/watermelon.webp'
+
 
 export const productsData = [
     {
-        id: 1,
-        title: 'Пълнители за сметана с вкус 640 грама Зеленя ябълка',
-        imgPath: greenApple,
-        priceOne: 40,
+        id: 5,
+        title: "Флакон с вкус - Боровинка",
+        imgPath: blubery,
+        priceOne: 45,
         priceTwo: 65,
-        priceDuptnica:45,
-        isBallon: false
+        priceDuptnica: 45,
     },
     {
-        id: 2,
-        title: "Пълнители за сметана с вкус 640 грама Пъпеш",
-        imgPath: melon,
-        priceOne: 40,
+        id: 6,
+        title: "Флакон с вкус - Лимон",
+        imgPath: lemon,
+        priceOne: 45,
         priceTwo: 65,
-        priceDuptnica:45,
-        isBallon: false
+        priceDuptnica: 45,
     },
     {
-        id: 3,
-        title: "Пълнители за сметана 640 без вкус",
-        imgPath: unflavoured,
-        priceOne: 40,
+        id: 7,
+        title: "Флакон с вкус - Диня",
+        imgPath: watermelon,
+        priceOne: 45,
         priceTwo: 65,
-        priceDuptnica:45,
-        isBallon: false
+        priceDuptnica: 45,
     },
+    // {
+    //     id: 1,
+    //     title: 'Пълнители за сметана с вкус 640 грама Зеленя ябълка',
+    //     imgPath: greenApple,
+    //     priceOne: 40,
+    //     priceTwo: 65,
+    //     priceDuptnica: 45,
+    //     isBallon: false
+    // },
+    // {
+    //     id: 2,
+    //     title: "Пълнители за сметана с вкус 640 грама Пъпеш",
+    //     imgPath: melon,
+    //     priceOne: 40,
+    //     priceTwo: 65,
+    //     priceDuptnica: 45,
+    //     isBallon: false
+    // },
+    // {
+    //     id: 3,
+    //     title: "Пълнители за сметана 640 без вкус",
+    //     imgPath: unflavoured,
+    //     priceOne: 40,
+    //     priceTwo: 65,
+    //     priceDuptnica: 45,
+    //     isBallon: false
+    // },
     {
         id: 4,
         title: "Пакет парти балони",
@@ -38,5 +66,6 @@ export const productsData = [
         priceOne: 20,
         priceTwo: 15,
         isBallon: true
-    }
+    },
+
 ]
