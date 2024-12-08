26f5358ea59fddec7c06401a59917c9f5bcf48e5
Stoyan Spasov <spasovstoyan@gmail.com>
2024-12-08 12:03:54 +0200

    enable React Router v7 future flags for startTransition and relativeSplatPath

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+import React from 'react';
 import './App.css';
-// import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import { Routes, Route } from "react-router-dom";
-// import HomePage from './components/pages/HomePage';
-// import LaughingGasPage from './components/pages/LaughingGasPage';
-// import IcePage from './components/pages/IcePage';
+import HomePage from './components/pages/HomePage';
+import LaughingGasPage from './components/pages/LaughingGasPage';
+import IcePage from './components/pages/IcePage';
 import ScrollToTop from './ScrollToTheTop';
-import UnderConstruction from './components/UnderConstruction/UnderConstruction';
 
 function App() {
   return (
     <>
       <ScrollToTop />
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
-        <Route exact path='/' element={<UnderConstruction />} />
-        {/* <Route path='/laughing-gas' element={<LaughingGasPage />} />
-        <Route path='/ice' element={<IcePage />} /> */}
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/laughing-gas' element={<LaughingGasPage />} />
+        <Route path='/ice' element={<IcePage />} />
       </Routes>
     </>
   );
