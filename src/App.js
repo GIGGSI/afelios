13790c640d42c92be1429bd77fe72a0e233d7c3b
Stoyan Spasov <spasovstoyan@gmail.com>
2025-01-11 22:21:58 +0200

    change pages

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import LaughingGasPage from './components/pages/LaughingGasPage';
 import IcePage from './components/pages/IcePage';
 import ScrollToTop from './ScrollToTheTop';
 
+import UnderConstruction from './components/UnderConstruction/UnderConstruction'
+
 function App() {
   return (
     <>
       <ScrollToTop />
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<HomePage />} />
-        <Route path='/laughing-gas' element={<LaughingGasPage />} />
-        <Route path='/ice' element={<IcePage />} />
+        <Route exact path='/' element={<UnderConstruction />} />
+        {/* <Route path='/laughing-gas' element={<LaughingGasPage />} />
+        <Route path='/ice' element={<IcePage />} /> */}
       </Routes>
     </>
   );
